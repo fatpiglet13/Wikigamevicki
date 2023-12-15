0d6cdb9a7ef47d00196f4d1415dc50c397ff1fb0
fatpiglet13 <144172135+fatpiglet13@users.noreply.github.com>
2023-12-15 17:52:49 +0000

    wikipedia-game-sc.py
    changed the visited

--- wikipedia-game-sc.py
+++ wikipedia-game-sc.py
@@ -20,19 +20,24 @@ def wikipedia_game_solver(start_page, target_page):
     print('Working on it...')
     start_time = time.time()
 
-    visited = []
-    queue = Queue()
-    path = []
+        visited.append(current_title)
+        current_page = wiki_wiki.page(current_title)
+        next_level = fetch_links(current_page)
 
-    visited.append(start_page.title)
+        for node in next_level:
+            if node not in visited:
+                queue.put(node)
+                parent[node] = current_title
 
-    while not queue.empty(): 
-        ddffd
-    #queue.get()
-    #queue.put(page)
-
-
-    # FINISH THE CODE HERE
+    child = target_page.title
+    while child != start_page.title:
+        path.append(child)
+        child = parent[child]
+    path.append(start_page.title)
+    path.reverse()
+        #get from queue (queue.get())
+        #add to visited 
+        #fetch the links
 
     end_time = time.time()
     print("This algorithm took", end_time-start_time, "seconds to run!")
